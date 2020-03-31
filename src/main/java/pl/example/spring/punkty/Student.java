1cda99f75109bafd08f6b9246813796f151ef6c9
Michał Lewicki <michlew241@student.polsl.pl>
2020-03-31 20:03:07 +0200

    Student class added

---
+++ src/main/java/pl/example/spring/punkty/Student.java
@@ -0,0 +1,15 @@
+package pl.example.spring.punkty;
+
+public class Student {
+    final long id;
+    final String name;
+    final String number;
+    final String grupa;
+
+    public Student(long id, String name, String number, String grupa) {
+        this.id = id;
+        this.name = name;
+        this.number = number;
+        this.grupa = grupa;
+    }
+}
