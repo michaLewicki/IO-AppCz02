17393e777f35aaf6bafb1b83d08587e6d20c6268
Michał Lewicki <michlew241@student.polsl.pl>
2020-04-10 17:15:35 +0200

    Persistence + changeNumber method added

--- src/main/java/pl/example/spring/punkty/StudentService.java
+++ src/main/java/pl/example/spring/punkty/StudentService.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Service;
 import pl.example.spring.punkty.db.StudentRepository;
 import pl.example.spring.punkty.db.StudentRow;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
@@ -19,6 +20,11 @@ public class StudentService {
 
     Student addStudent(final NewStudent newStudent) {
         return this.repository.save(new StudentRow( newStudent.name, newStudent.number, newStudent.grupa)).toStudent();
+    }
+
+    Optional<Student> changeNumber(long studentId, String newNumber) {
+        final Optional<StudentRow> student = this.repository.findById(studentId);
+        return student.map(c -> { c.setNumber(newNumber); return c.toStudent(); });
     }
 
 }
